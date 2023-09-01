c669e321dc559fc084b4d3af8dcc6d6d604dccdc
Giacomo Granata <72440728+GiacomoGranata@users.noreply.github.com>
2023-09-01 17:26:30 +0200

    Add files via upload

---
+++ cart_pole_snn_v5.py
@@ -0,0 +1,353 @@
+from cma import CMAEvolutionStrategy as cmaes
+import gym
+from spiking_network_v4 import SNN
+import numpy as np
+import functools
+from random import Random
+from multiprocessing import Pool
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random
+from snntorch import spikegen
+import torch
+import math
+
+
+def min0_max1(x):
+    if x < 0:
+        print("input too low")
+        return 0
+    if x > 1:
+        print("input too high")
+        return 1
+    return x
+
+def closest_binary(n, m):
+    binary_number = []
+    for i in range(n):
+        if m >= 2**(n - 1 - i):
+            binary_number.append(1)
+            m -= 2**(n - 1 - i)
+        else:
+            binary_number.append(0)
+    return binary_number
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def inputs_resizing(inputs, params):
+    output = []
+    for i in range(len(inputs)):
+        output.append(inputs[i]*params[-1-i])
+    return output
+
+def double_encoding(inputs):
+    output = []
+    for i in inputs:
+        if i >= 0:
+            output.append(0)
+            output.append(i)
+        else:
+            output.append(-i)
+            output.append(0)
+    return output
+
+
+def rate_code(inputs, n_timesteps):
+    double_inputs = double_encoding(inputs)
+    scaled_inputs = []
+    scaled_inputs.append(math.tanh(double_inputs[0]))
+    scaled_inputs.append(math.tanh(double_inputs[1]))
+    scaled_inputs.append(math.tanh(double_inputs[2]))
+    scaled_inputs.append(math.tanh(double_inputs[3]))
+    scaled_inputs.append(math.tanh(double_inputs[4]))
+    scaled_inputs.append(math.tanh(double_inputs[5]))
+    scaled_inputs.append(math.tanh(double_inputs[6]))
+    scaled_inputs.append(math.tanh(double_inputs[7]))
+    encoded_inputs = spikegen.rate(torch.tensor(scaled_inputs), num_steps = n_timesteps)
+    return encoded_inputs.tolist()
+
+
+def latency_code(inputs, n_timesteps):
+    double_inputs = double_encoding(inputs)
+    scaled_inputs = []
+    scaled_inputs.append(math.tanh(double_inputs[0]))
+    scaled_inputs.append(math.tanh(double_inputs[1]))
+    scaled_inputs.append(math.tanh(double_inputs[2]))
+    scaled_inputs.append(math.tanh(double_inputs[3]))
+    scaled_inputs.append(math.tanh(double_inputs[4]))
+    scaled_inputs.append(math.tanh(double_inputs[5]))
+    scaled_inputs.append(math.tanh(double_inputs[6]))
+    scaled_inputs.append(math.tanh(double_inputs[7]))
+    encoded_inputs = spikegen.latency(torch.tensor(scaled_inputs), num_steps = n_timesteps, normalize=True, linear=True)
+    return encoded_inputs.tolist()
+
+
+def no_code(inputs, n_timesteps):
+    double_inputs = double_encoding(inputs)
+    return [double_inputs.copy() for _ in range(n_timesteps)]
+
+'''def no_code(inputs, n_timesteps):
+    double_inputs = double_encoding(inputs)
+    scaled_inputs = []
+    scaled_inputs.append(math.tanh(double_inputs[0]))
+    scaled_inputs.append(math.tanh(double_inputs[1]))
+    scaled_inputs.append(math.tanh(double_inputs[2]))
+    scaled_inputs.append(math.tanh(double_inputs[3]))
+    scaled_inputs.append(math.tanh(double_inputs[4]))
+    scaled_inputs.append(math.tanh(double_inputs[5]))
+    scaled_inputs.append(math.tanh(double_inputs[6]))
+    scaled_inputs.append(math.tanh(double_inputs[7]))
+    return [scaled_inputs.copy() for _ in range(n_timesteps)]'''
+
+
+def phase_code(inputs, n_timesteps):
+    
+    double_inputs = double_encoding(inputs)
+    scaled_inputs = []
+    scaled_inputs.append(math.tanh(double_inputs[0]))
+    scaled_inputs.append(math.tanh(double_inputs[1]))
+    scaled_inputs.append(math.tanh(double_inputs[2]))
+    scaled_inputs.append(math.tanh(double_inputs[3]))
+    scaled_inputs.append(math.tanh(double_inputs[4]))
+    scaled_inputs.append(math.tanh(double_inputs[5]))
+    scaled_inputs.append(math.tanh(double_inputs[6]))
+    scaled_inputs.append(math.tanh(double_inputs[7]))
+    
+    phase_coded_inputs = []
+
+    max_representabile_number = 2**n_timesteps
+    scaled_inputs[0] *= max_representabile_number
+    scaled_inputs[1] *= max_representabile_number
+    scaled_inputs[2] *= max_representabile_number
+    scaled_inputs[3] *= max_representabile_number
+
+    phase_coded_inputs.append(closest_binary(n_timesteps, scaled_inputs[0]))
+    phase_coded_inputs.append(closest_binary(n_timesteps, scaled_inputs[1]))
+    phase_coded_inputs.append(closest_binary(n_timesteps, scaled_inputs[2]))
+    phase_coded_inputs.append(closest_binary(n_timesteps, scaled_inputs[3]))
+
+    phase_coded_inputs_transposed = list(zip(*phase_coded_inputs))
+
+    return phase_coded_inputs_transposed
+
+
+def eval(x, render=False):
+    cumulative_rewards = []
+    task = gym.make("CartPole-v1")
+    agent = SNN([8, 4, 4, 2], 20)
+    agent.set_params(x)
+    for i in range(100):
+        cumulative_rewards.append(0)
+        done = False
+        task.seed(i)
+        obs = task.reset()
+        counter = 0
+        while not done:
+            output = agent.activate(no_code(inputs_resizing(obs, x), 30))
+
+            # arr[output]+=1
+            if render:
+                task.render()
+            obs, rew, done, _ = task.step(np.argmax(output))
+            cumulative_rewards[-1] += rew
+        counter += 1
+
+    return -sum(cumulative_rewards) / 100
+
+def get_pruning_mask(snn, weights, prune_ratio):
+    mask = np.ones_like(weights)
+    c = 0
+    snn.set_params(weights)
+    for i in range(1,len(snn.neurons)):
+        for j in range(len(snn.neurons[i])):
+            num_to_prune = round(prune_ratio/100 * (len(snn.neurons[i-1])+1))
+            neuron_weights = snn.weights[i-1][j]
+            sorted_weights = np.sort(np.abs(neuron_weights))
+            threshold = sorted_weights[num_to_prune-1]
+            neuron_mask = np.ones_like(neuron_weights)
+            neuron_mask[np.abs(neuron_weights) <= threshold] = 0
+            for l in range(len(snn.neurons[i-1])):
+                    mask[c] = neuron_mask[l]
+                    c += 1
+    return mask
+
+
+def generator(random, args):
+    return np.asarray([random.uniform(args["pop_init_range"][0],
+                                      args["pop_init_range"][1])
+                       for _ in range(args["num_vars"])])
+
+
+def generator_wrapper(func):
+    @functools.wraps(func)
+    def _generator(random, args):
+        return np.asarray(func(random, args))
+
+    return _generator
+
+
+def parallel_val(candidates):
+    with Pool(20) as p:
+        return p.map(eval, candidates)
+
+
+def save_scores(average_scores_history, best_scores_history, file_prefix='mountain_car_'):
+    with open(f'{file_prefix}average_scores.txt', 'w') as f:
+        f.write(' '.join(map(str, average_scores_history)))
+    with open(f'{file_prefix}best_scores.txt', 'w') as f:
+        f.write(' '.join(map(str, best_scores_history)))
+
+
+def plot_fitnesses(average_scores_history, best_scores_history):
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(24,12))
+    colors = cm.rainbow(np.linspace(0, 1, len(average_scores_history)))
+
+    average_scores_history_concat = sum(average_scores_history, [])
+    best_scores_history_concat = sum(best_scores_history, [])
+
+    for i, fitness in enumerate(average_scores_history):
+        x = list(range(sum(map(len, average_scores_history[:i])), sum(map(len, average_scores_history[:i+1]))))
+        axes[0].plot(x, fitness, label=f'Density {round((0.8 ** i) * 100, 1)}%', color=colors[i])
+    axes[0].axhline(y=-475, linestyle='--', color='black', label='Successful score')
+    axes[0].legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=1)
+    axes[0].set_xlabel('Numero di generazioni')
+    axes[0].set_ylabel('Punteggio medio')
+    axes[0].set_title('Average scores')
+    axes[0].set_ylim(bottom=min(min(best_scores_history_concat), min(average_scores_history_concat), -200)-30, top=max(max(best_scores_history_concat), max(average_scores_history_concat), -475)+30)
+
+    for i, fitness in enumerate(best_scores_history):
+        x = list(range(sum(map(len, best_scores_history[:i])), sum(map(len, best_scores_history[:i+1]))))
+        axes[1].plot(x, fitness, label=f'Density {round((0.8 ** i) * 100, 1)}%', color=colors[i])
+    axes[1].axhline(y=-475, linestyle='--', color='black', label='Successful score')
+    axes[1].legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=1)
+    axes[1].set_xlabel('Numero di generazioni')
+    axes[1].set_ylabel('Punteggio migliore')
+    axes[1].set_title('Best guy scores')
+    axes[1].set_ylim(bottom=min(min(best_scores_history_concat), min(average_scores_history_concat), -475)-30, top=max(max(best_scores_history_concat), max(average_scores_history_concat), -475)+30)
+
+    plt.subplots_adjust(right=0.82)
+    
+    plt.savefig(f'./CartPole_SNN_NoCode_PruningIteration#{len(average_scores_history)-1}.png')
+
+
+if __name__ == "__main__":
+    args = {}
+    fka = SNN([8, 4, 4, 2], 20)
+
+    args["num_vars"] = fka.nweights + fka.nthresholds + fka.nbetas + len(fka.neurons[0])
+    args["max_total_generations"] = 500
+    args["added_generations"] = 50
+    args["max_initial_generations"] = 100
+    args["sigma"] = 1.0
+    args["pop_size"] = 4
+    args["num_offspring"] = 20
+    args["pop_init_range"] = [0, 1]
+
+    random = Random(0)
+
+    es = cmaes(generator(random, args),
+               args["sigma"],
+               {'popsize': args["num_offspring"],
+                'seed': 0,
+                'CMA_mu': args["pop_size"],
+                'bounds': [0, np.inf]})
+
+    '''es = cmaes(generator(random, args),
+               args["sigma"],
+               {'popsize': args["num_offspring"],
+                'seed': 0,
+                'CMA_mu': args["pop_size"]})'''
+
+    gen = 0
+    n = 0
+    pruning_mask = np.ones(args["num_vars"])
+    initial_candidates = es.ask()
+    candidates = initial_candidates
+    fitnesses = np.zeros(args["num_offspring"])
+    current_prune_ratio = 0
+
+    average_scores_history = []
+    best_scores_history = []
+    
+    while gen <= args["max_total_generations"]-args["max_initial_generations"]:
+        average_scores_history.append([])
+        best_scores_history.append([])
+        for i in range(args["max_initial_generations"]+ args["added_generations"]*n):
+            if i == 0:
+                gen = 0
+                candidates = es.ask()
+                # apply the pruning mask to the whole offspring
+                candidates = np.multiply(initial_candidates, np.tile(pruning_mask, (args["num_offspring"],1)))
+            else:
+                candidates = es.ask()
+                # apply the pruning mask to the whole offspring
+                candidates = np.multiply(candidates, np.tile(pruning_mask, (args["num_offspring"],1)))
+            fitnesses = parallel_val(candidates)
+            average_scores_history[n].append(np.mean(fitnesses))
+            best_scores_history[n].append(min(fitnesses))
+            print("Network density: "+str((0.8**(n))*100)+"%, Gen "+str(gen+1)+"  "+str(min(fitnesses))+"  "+str(np.mean(fitnesses)))
+            es.tell(candidates, fitnesses)
+
+            best_guy = es.best.x
+            best = SNN([8, 4, 4, 2], 20)
+            best.set_params(best_guy)
+            with open("thresholds-biases.txt", "a") as file:
+                file.write("GEN " + str(gen+1) + "\n")
+                file.write("thresholds:\n")
+                d = 0
+                for neuron in best.neurons:
+                    for i in range(len(neuron)):
+                        file.write(str(best.neurons[d][i].threshold) + "\n")
+                    d += 1
+                file.write("\n")
+                file.write("betas:\n")
+                d = 0
+                for neuron in best.neurons:
+                    for i in range(len(neuron)):
+                        file.write(str(best.neurons[d][i].beta) + "\n")
+                    d += 1
+                file.write("\n\n")
+
+            gen += 1
+        print()
+        plot_fitnesses(average_scores_history, best_scores_history)
+        # calculate the pruning mask on the best guy in the current generation
+        best_guy = es.best.x
+        print(best_guy)
+        best = SNN([8, 4, 4, 2], 20)
+        best.set_params(best_guy)
+        
+
+        '''
+        print("thresholds:")
+        d = 0
+        for neuron in best.neurons:
+            for i in range(len(neuron)):
+                print(best.neurons[d][i].threshold)
+            d += 1
+        print()
+        print("betas:")
+        d = 0
+        for neuron in best.neurons:
+            for i in range(len(neuron)):
+                print(best.neurons[d][i].beta)
+            d += 1
+        '''
+            
+        current_prune_ratio += (100-current_prune_ratio)*fka.prune_ratio/100
+        pruning_mask = get_pruning_mask(fka, best_guy, current_prune_ratio)
+        
+        n += 1
+    final_pop = np.asarray(es.ask())
+    final_pop_fitnesses = np.asarray(parallel_val(final_pop))
+    
+    best_guy = es.best.x
+    best_fitness = es.best.f
+
+    save_scores(average_scores_history, best_scores_history)
+
+    with open("./nn_cartpole_SNN_nocode"+str(best_fitness)+".pkl", "wb") as f:
+        pickle.dump(best_guy, f)
---
+++ spiking_network_v5.py
@@ -0,0 +1,78 @@
+import numpy as np
+import torch
+from snntorch import spikegen
+import math
+
+def threshold_norm(threshold):
+    if threshold >= 0:
+        return (min(threshold, 6) + 6)
+    else:
+        return (max(threshold, -6) + 6)
+    
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+class LifNeuron():
+    def __init__(self, threshold, beta):
+        self.threshold = threshold
+        self.beta = beta
+        self.membrane_potential = 0
+        self.spike = 0
+
+    def activate(self, input):
+        self.membrane_potential = self.beta*self.membrane_potential + input
+        if self.membrane_potential >= self.threshold:
+            self.spike = 1
+            self.membrane_potential -= self.threshold
+        else:
+            self.spike = 0
+
+class SNN():
+    def __init__(self, nodes, prune_ratio):
+        self.neurons = [[LifNeuron(1, 0.5) for i in range(node)] for node in nodes]
+        self.nthresholds = 0
+        for i in range(len(nodes)):
+            for j in range(nodes[i]):
+                self.nthresholds += 1
+        self.nbetas = self.nthresholds
+        self.nweights = 0
+        for i in range(len(nodes) - 1):
+            self.nweights += nodes[i]*nodes[i+1] # weights
+            self.nweights += nodes[1+i] # biases
+        self.prune_ratio = prune_ratio
+        self.weights = [[] for _ in range(len(self.neurons) - 1)]
+
+    def activate(self, inputs):
+        output = [0 for i in range(len(self.neurons[-1]))]
+        for k in range(len(inputs)):
+            for i in range(len(inputs[k])):
+                self.neurons[0][i].activate(inputs[k][i])
+            for i in range(1, len(self.neurons)): 
+                for j in range(len(self.neurons[i])):
+                    sum = self.weights[i - 1][j][0]
+                    for k in range(0, len(self.neurons[i - 1])):
+                        sum += self.neurons[i - 1][k].spike * self.weights[i - 1][j][k]
+                    self.neurons[i][j].activate(sum)
+            output = np.add(output, np.array([self.neurons[-1][i].spike for i in range(len(self.neurons[-1]))]))
+        return output
+
+    def set_params(self, weights):
+        c = 0
+        d = 0
+        for i in range(1, len(self.neurons)):
+            self.weights[i - 1] = [[0 for _ in range(len(self.neurons[i - 1]) + 1)] for __ in range(len(self.neurons[i]))]
+            for j in range(len(self.neurons[i])):
+                for k in range(len(self.neurons[i - 1]) + 1):
+                    self.weights[i - 1][j][k] = weights[c]
+                    c += 1
+        thresholds = [weights[self.nweights+i] for i in range(0, self.nthresholds)]
+        betas = [weights[self.nweights+self.nthresholds+i] for i in range(0, self.nbetas)]
+        for i in range(len(self.neurons)):
+            for j in range(len(self.neurons[i])):
+                self.neurons[i][j].threshold = thresholds[d]
+                d += 1
+        d = 0
+        for i in range(len(self.neurons)):
+            for j in range(len(self.neurons[i])):
+                self.neurons[i][j].beta = sigmoid(betas[d])
+                d += 1
